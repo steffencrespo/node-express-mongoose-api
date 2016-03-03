c8ff3ae235783dd93688c6f5949d25c490b5637d
Leonardo Steffen <steffencrespo@hotmail.com>
2016-03-03 11:32:35 -0800

    adds little image to serve as a delete button - not ready yet. Also adds link to the complete place json coming from db

--- app/public/client.js
+++ app/public/client.js
@@ -5,7 +5,10 @@ $(function(){
     function appendToList(places){
         var list = [];
         for (var i in places){
-            list.push($('<li>', {text: places[i].name}));
+            place = places[i];
+            content = '<a href="/places/'+place+'">'+place.name+'</a> '+
+                '<a href="#" place-block="'+place+'"><img src="delete.jpg" height="10" width="10"></a>';
+            list.push($('<li>', {html: content}));
         }
         $('.place-list').append(list);
     }
